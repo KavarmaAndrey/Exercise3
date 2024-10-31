ba3c463d0a8ea9bd7574ad089ae3654353134536
Andrey Kavarma <kavarmaa@ukr.net>
2024-10-31 22:33:38 +0200

    Changed the text into WorckWithGid

--- src/kavarmaAndrey/WorckWithGid.java
+++ src/kavarmaAndrey/WorckWithGid.java
@@ -2,7 +2,7 @@ package kavarmaAndrey;
 
 public class WorckWithGid {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
 
     }
 }
